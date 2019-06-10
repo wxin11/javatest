6ac8b553a4dca332eecefd71acbd2dbc6d9455a0
wxin11 <weixin_zm@qq.com>
2019-06-11 00:54:36 +0800

    add mysql test

---
+++ mysqltest/src/mysqltest/Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ *
+ * author weixin
+ * 2019年6月11日 上午12:45:42
+ *
+ */
+
+package mysqltest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Mysql Test");
+
+	}
+
+}
