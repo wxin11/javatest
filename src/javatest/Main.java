8765b7f9373392d26e01b566930c232d33810100
wxin11 <weixin.zm@qq.com>
2019-06-10 16:58:04 +0800

    new

---
+++ src/javatest/Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ *
+ * author weixin
+ * 2019Äê6ÔÂ10ÈÕ
+ *
+ */
+
+package javatest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("hello java");
+
+	}
+
+}
